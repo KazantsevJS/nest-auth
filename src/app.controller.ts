c28a52270cb3eb8eaee77a248de29a32629f5a68
pauzhetsky <kazancev.alex2020@gmail.com>
2025-09-06 23:56:49 +0300

    refactor: clean up AppModule and AppController, remove unused AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('time')
+  getTime(): { time: string } {
+    return { time: new Date().toISOString() };
   }
 }
