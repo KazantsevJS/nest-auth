44a42d22315e1b13d828cbd3d3b341c6c180a272
pauzhetsky <kazancev.alex2020@gmail.com>
2025-09-07 04:47:45 +0300

    fix: authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -19,17 +20,21 @@ interface AuthRequest extends Request {
   user: RequestUser;
 }
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create new user' })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get current user info' })
   async getMe(@Request() req: AuthRequest) {
     return this.usersService.findById(req.user.userId);
   }
