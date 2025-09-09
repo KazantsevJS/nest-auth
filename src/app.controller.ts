37f2dad75704648d1a18fb9e0afacb71d7cfd2d5
pauzhetsky <kazancev.alex2020@gmail.com>
2025-09-09 18:33:12 +0300

    initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('time')
-  getTime(): { time: string } {
-    return { time: new Date().toISOString() };
+  @Get()
+  getHello(): string {
+    return 'Auth API is running!';
   }
 }
