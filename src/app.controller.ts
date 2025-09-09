205993e8156f7134f8501cf03e22908d24e1d058
pauzhetsky <kazancev.alex2020@gmail.com>
2025-09-09 17:39:14 +0300

    feat: clear main

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get('time')
-  getTime(): { time: string } {
-    return { time: new Date().toISOString() };
-  }
-}
--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class LoginDto {
-  @IsEmail({}, { message: 'Only valid email adress' })
-  email: string;
-
-  @IsString({ message: 'Password must be a string' })
-  password: string;
-}
